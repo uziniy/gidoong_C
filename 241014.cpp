95df2c62827c9acb9cadad17649c1feb98d93144
woojin <wjchoi.son@gmail.com>
2024-10-14 23:20:03 +0900

    241014

---
+++ 241014.cpp
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  기동이
+//
+//  Created by 최우진 on 10/9/24.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    char **ss = malloc(sizeof(char*)*20); //char ss[20] -> 메모리 입장에서 똑같이 20칸 생긴거잖아?
+    for (int i=0; i<20; i++) {
+        
+    }
+    
+    
+    return 0;
+}
